8e51f37d98341369a7294dccf370fababa6b2cd0
therab6it <97642829+therab6it@users.noreply.github.com>
2022-01-26 16:48:34 -0800

    Update MarkdownParseTest.java
    commented all lines

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,9 @@
-import static org.junit.Assert.*; // imports junit lib
-import org.junit.*;               // imports junit library
+import static org.junit.Assert.*; // imports junit assert library
+import org.junit.*;               // imports all junit library
 
-public class MarkdownParseTest {  //
-    @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+public class MarkdownParseTest {  // file class
+    @Test                         // indicated a test
+    public void addition() {      // void method for Junit tester
+        assertEquals(2, 1 + 1);   // checks if 2 is equal to 1+1
     }
 }
